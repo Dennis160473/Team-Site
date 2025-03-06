5429c6883c9cc519ee8c7c561ba835d4ed8c38bf
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-06 09:29:59 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,16 +1,6 @@
-function onClickButton() {
-    const boxDiv = document.getElementById("box");
-    boxDiv.innerHTML = "<button>Im a button</button>";
-    const currentBgColor = boxDiv.style.backgroundColor;
-    console.log("box div background", currentBgColor);
-    boxDiv.style.backgroundColor = "green";
-  }
-  
-  function onLogin() {
+function onLogin() {
     const emailTI = document.getElementById("emailti").value;
     const password = document.getElementById("passwordti").value;
-    console.log("My Credentials", emailTI, password);
-    alert(
-      `Der Benutzer hat sich erfolgreich mit ${emailTI} und ${password} angemeldet`
-    );
+    
+    alert(`Der Benutzer hat sich erfolgreich mit ${emailTI} und ${password} angemeldet`);
   }
