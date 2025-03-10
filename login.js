28cf0a66f58d187ae9709521469eb1468a22280b
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-10 09:28:26 +0100

    Update login.js

--- login.js
+++ login.js
@@ -4,6 +4,18 @@ function onLogin() {
     
     alert(`Der Benutzer hat sich erfolgreich mit ${emailTI} und ${password} angemeldet`);
   }
-let mylist = [Andrè, Dennis, Hans-Jürgen, Karsten]
 
-let spliced_list = mylist.splice()
+function remove_second_element(liste){
+  liste.splice(1, 1)
+}
+let mylist = [1, 2, 3, 4, 5]
+let deleted_element = remove_second_element(mylist)
+
+
+
+
+// Alternative
+let spliced_list = mylist.splice(1, 1)
+
+console.log(mylist)
+console.log(spliced_list)
