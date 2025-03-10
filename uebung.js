644f13de2cdb53b845155f838c3d8daca736a93a
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-10 14:15:27 +0100

    updateuebung.js

--- uebung.js
+++ uebung.js
@@ -33,6 +33,39 @@ function setUserInputList(){
     my_list = my_list.concat(textList)
     setListContent()
 }
+//Hausaufgabe10032025
+function addItem() {
+    // Den Wert des Eingabefelds abrufen
+    const itemInput = document.getElementById('itemInput');
+    const itemValue = itemInput.value;
+
+    // Überprüfen, ob das Eingabefeld nicht leer ist
+    if (itemValue.trim() !== '') {
+        // Ein neues Listenelement erstellen
+        const li = document.createElement('li');
+        li.textContent = itemValue;
+
+        // Das neue Element zur Liste hinzufügen
+        const itemList = document.getElementById('itemList');
+        my_list = my_list.appendChild(itemInput)
+
+        // Das Eingabefeld zurücksetzen
+        itemInput.value = '';
+    } else {
+        alert('Bitte geben Sie ein Item ein.');
+    }
+
+    setListContent()
+}
+// Löschen des Elementes
+function deleteItem()
+let lastElement = my_list.pop();
+
+return lastElement
+//
+
+
+
 
 
 
