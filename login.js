3677a083d0f3ba3efe896c84f868a3399bb04681
Dennis Angermann <dennis.angermann@tn.techstarter.de>
2025-03-10 09:36:01 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,22 +1,21 @@
+// ruft ID des Users auf und vergleicht Daten. Gibt Boolean True zurück.
 function onLogin() {
     const emailTI = document.getElementById("emailti").value;
     const password = document.getElementById("passwordti").value;
     
     alert(`Der Benutzer hat sich erfolgreich mit ${emailTI} und ${password} angemeldet`);
   }
-
+// Löscht definierte Elemente aus dem Array
 function remove_second_element(liste){
   liste.splice(1, 1)
 }
 let mylist = [1, 2, 3, 4, 5]
 let deleted_element = remove_second_element(mylist)
-
-// Alternative
+// Alternative zum Löschen
 let spliced_list = mylist.splice(1, 1)
 
 console.log(mylist)
 console.log(spliced_list)
-
 // filtern des Arrays
 const ages = [32, 33, 16, 40];
 const result = ages.filter(checkAdult);
